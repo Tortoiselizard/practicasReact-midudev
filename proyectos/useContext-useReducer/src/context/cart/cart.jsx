278e184d6d068d954b58f073f5e938089f0de8ce
troy <troy00pernia@gmail.com>
2023-10-17 15:49:20 -0400

    add context/cart/

--- proyectos/useContext-useReducer/src/context/cart/cart.jsx
+++ proyectos/useContext-useReducer/src/context/cart/cart.jsx
@@ -1,13 +1,45 @@
 import { useState, createContext } from 'react'
+import { products } from '../../mocks/products.json'
 
 export const CartContext = createContext()
 
 export function CartProvider ({ children }) {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(products.slice(0, 5).map(item => ({
+    title: item.title,
+    image: item.images[0],
+    quantity: 1
+  })))
+
+  function addItem (event) {
+    const newCart = structuredClone(cart)
+    const id = Number(event.target.name)
+    const item = products.filter(item => item.id === id)[0]
+    console.log('item:', item)
+    newCart.push({
+      title: item.title,
+      image: item.images[0],
+      quantity: 1
+    })
+    setCart(newCart)
+  }
+
+  function handleChange (event) {
+    const { name, value } = event.target
+    const newCart = structuredClone(cart)
+    if (value <= 0) {
+      newCart.splice(Number(name), 1)
+    } else {
+      newCart[name].quantity = Number(value)
+    }
+    setCart(newCart)
+  }
+
   return (
     <CartContext.Provider value={{
       cart,
-      setCart
+      setCart,
+      handleChange,
+      addItem
     }}
     >
       {children}
